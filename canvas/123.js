888e4eebfd907173f2be98d7c1813cc5fd5d1248
夜尘Dakeniz <53067795+nightlye@users.noreply.github.com>
2022-08-29 23:12:41 +0800

    Add files via upload
    禁漫天堂 canvas解密图片

---
+++ canvas/123.js
@@ -0,0 +1,9 @@
+<a href="/photo/373028" data-album="373028">
+    {' '}
+    <li class="current_series">
+        第1話 在我的懷裡嬌喘吧～男裝天團偶像的戀愛謊言/俺の腕の中で歌え！～男装アイドルの恋と嘘{' '}
+        <span class="hidden-xs" style="float: right;">
+            2022-08-29
+        </span>
+    </li>{' '}
+</a>;
---
+++ canvas/site_js.js
@@ -0,0 +1,218 @@
+var _0x40b85d = _0x38b8;
+function _0x38b8(_0x4f4f8a, _0x31d11e) {
+    var _0x52c08c = _0x52c0();
+    return (
+        (_0x38b8 = function (_0x38b8ba, _0x5ad2e) {
+            _0x38b8ba = _0x38b8ba - 0x1c8;
+            var _0x4c7665 = _0x52c08c[_0x38b8ba];
+            return _0x4c7665;
+        }),
+        _0x38b8(_0x4f4f8a, _0x31d11e)
+    );
+}
+
+var A = function (_0x10a1ce, _0x19ce93) {
+    var _0x433955 = _0x38b8,
+        _0x495178 = _0x10a1ce();
+    while (!![]) {
+        try {
+            var _0x3600c5 =
+                parseInt(_0x433955(0x1d2)) / 0x1 +
+                -parseInt(_0x433955(0x1ce)) / 0x2 +
+                -parseInt(_0x433955(0x1cc)) / 0x3 +
+                -parseInt(_0x433955(0x1c9)) / 0x4 +
+                (parseInt(_0x433955(0x1d6)) / 0x5) * (parseInt(_0x433955(0x1c8)) / 0x6) +
+                (parseInt(_0x433955(0x1d0)) / 0x7) * (-parseInt(_0x433955(0x1d1)) / 0x8) +
+                parseInt(_0x433955(0x1ca)) / 0x9;
+            if (_0x3600c5 === _0x19ce93) break;
+            else _0x495178['push'](_0x495178['shift']());
+        } catch (_0x4685b1) {
+            _0x495178['push'](_0x495178['shift']());
+        }
+    }
+};
+var B = function (_0x5d76b6, _0x1cab3c, _0x22a9ed, _0x26aad7, _0x10389c, _0x4652e9) {
+    var _0x5aac6c = _0x38b8;
+    _0x10389c = function (_0x45f136) {
+        var _0x5c3b73 = _0x38b8;
+        return (
+            (_0x45f136 < _0x1cab3c ? '' : _0x10389c(parseInt(_0x45f136 / _0x1cab3c))) +
+            ((_0x45f136 = _0x45f136 % _0x1cab3c) > 0x23
+                ? String[_0x5c3b73(0x1d3)](_0x45f136 + 0x1d)
+                : _0x45f136[_0x5c3b73(0x1cf)](0x24))
+        );
+    };
+    if (!''[_0x5aac6c(0x1cd)](/^/, String)) {
+        while (_0x22a9ed--)
+            _0x4652e9[_0x10389c(_0x22a9ed)] = _0x26aad7[_0x22a9ed] || _0x10389c(_0x22a9ed);
+        (_0x26aad7 = [
+            function (_0xe13122) {
+                return _0x4652e9[_0xe13122];
+            },
+        ]),
+            (_0x10389c = function () {
+                var _0x132acb = _0x5aac6c;
+                return _0x132acb(0x1d5);
+            }),
+            (_0x22a9ed = 0x1);
+    }
+    while (_0x22a9ed--)
+        if (_0x26aad7[_0x22a9ed])
+            _0x5d76b6 = _0x5d76b6[_0x5aac6c(0x1cd)](
+                new RegExp('\x5cb' + _0x10389c(_0x22a9ed) + '\x5cb', 'g'),
+                _0x26aad7[_0x22a9ed]
+            );
+    return _0x5d76b6;
+};
+
+function _0x52c0() {
+    var _0x538c88 = [
+        'replace',
+        '1492718PYyDaY',
+        'toString',
+        '7YBbWJi',
+        '7961736HblkLG',
+        '768682xrmImB',
+        'fromCharCode',
+        'split',
+        '\x5cw+',
+        '10gFoVzb',
+        '784296okcOnT',
+        '3262492sTEclD',
+        '20873943ihxWcQ',
+        'w\x201a(e,t,a,n,d){n||(n=!1),d||(d=!1),e.O.1b(\x22.1c\x22)>0||B(t)<B(a)||\x221\x22==d?\x22P\x22===e.j.z&&(e.j.z=\x22F\x22):1==n||0==e.1d?v.C(e.D).1e=w(){G(e)}:G(e)}w\x20G(e){b\x20t;1f==e.Q?(t=v.H(\x22E\x22),e.1g(t)):t=v.C(e.D).Q;b\x20a=t.R(\x22S\x22),n=e.k,d=e.T,i=e.U;t.k=d,t.V=i,(n>e.I.W||0==n)&&(n=e.I.W),t.j.k=n+\x22J\x22,t.j.z=\x22F\x22;b\x20r=v.C(e.D).I;r=(r=r.D.X(\x22.\x22))[0];Y(b\x20s=K(Z,r),l=B(i%s),o=d,m=0;m<s;m++){b\x20c=11.13(i/s),g=c*m,h=i-c*(m+1)-l;0==m?c+=l:g+=l,a.15(e,0,h,o,c,0,g,o,c)}$(e).1h(\x221i\x22)}w\x201j(e,t,a){b\x20n=v.C(\x221k\x22);17(n.1l=w(){n.j.z=\x22P\x22},t||(t=!1),t){b\x20d;0!=n.x(\x22A\x22).L&&(y=n.x(\x22A\x22)[0],n.1m(y)),0==n.x(\x22E\x22).L?(d=v.H(\x22E\x22),n.19(d)):d=n.x(\x22E\x22)[0];b\x20i=d.R(\x22S\x22),r=e.T,s=e.U;d.k=r,d.V=s,M.N.k<1n?d.j.k=M.N.k+\x22J\x22:r>M.N.k&&(d.j.k=r/2+\x22J\x22),d.j.1o=\x221p\x22;b\x20l=a.X(\x22.\x22);l=l[0];Y(b\x20o=K(Z,l),m=B(s%o),c=0;c<o;c++){b\x20g=r,h=11.13(s/o),p=h*c,u=s-h*(c+1)-m;0==c?h+=m:p+=m,i.15(e,0,u,g,h,0,p,g,h)}}1q{b\x20y;0==n.x(\x22A\x22).L?(y=v.H(\x22A\x22),n.19(y)):y=n.x(\x22A\x22)[0],y.O=e}n.j.z=\x22F\x22}w\x20K(e,t){b\x20a=10;17(e>=1r){b\x20n=e+t;1s(n=(n=(n=1t(n)).1u(-1)).1v(),n%=10){f\x200:a=2;q;f\x201:a=4;q;f\x202:a=6;q;f\x203:a=8;q;f\x204:a=10;q;f\x205:a=12;q;f\x206:a=14;q;f\x207:a=16;q;f\x208:a=18;q;f\x209:a=1w}}1x\x20a}',
+        '620457RlCSsa',
+    ];
+    _0x52c0 = function () {
+        return _0x538c88;
+    };
+    return _0x52c0();
+}
+A(_0x52c0, 0x8eecf);
+B(
+    _0x40b85d(0x1cb),
+    0x3e,
+    0x60,
+    '|||||||||||var||||case||||style|width||||||break|||||document|function|getElementsByTagName||display|img|parseInt|getElementById|id|canvas|block|onImageLoaded|createElement|parentNode|px|get_num|length|window|screen|src|none|nextElementSibling|getContext|2d|naturalWidth|naturalHeight|height|offsetWidth|split|for|aid||Math||floor||drawImage||if||append|scramble_image|indexOf|gif|complete|onload|null|after|addClass|hide|showBig|popup|onclick|removeChild|991|padding|10px|else|268850|switch|md5|substr|charCodeAt|20|return'[
+        _0x40b85d(0x1d4)
+    ]('|'),
+    0x0,
+    {}
+);
+//B返回了一段函数
+
+function scramble_image(e, t, a, n, d) {
+    n || (n = !1),
+        d || (d = !1),
+        e.src.indexOf('.gif') > 0 || parseInt(t) < parseInt(a) || '1' == d
+            ? 'none' === e.style.display && (e.style.display = 'block')
+            : 1 == n || 0 == e.complete
+            ? (document.getElementById(e.id).onload = function () {
+                  onImageLoaded(e);
+              })
+            : onImageLoaded(e);
+}
+function onImageLoaded(e) {
+    var t;
+    null == e.nextElementSibling
+        ? ((t = document.createElement('canvas')), e.after(t))
+        : (t = document.getElementById(e.id).nextElementSibling);
+    var a = t.getContext('2d'),
+        n = e.width,
+        d = e.naturalWidth,
+        i = e.naturalHeight;
+    (t.width = d),
+        (t.height = i),
+        (n > e.parentNode.offsetWidth || 0 == n) && (n = e.parentNode.offsetWidth),
+        (t.style.width = n + 'px'),
+        (t.style.display = 'block');
+    var r = document.getElementById(e.id).parentNode;
+    r = (r = r.id.split('.'))[0];
+    for (var s = get_num(aid, r), l = parseInt(i % s), o = d, m = 0; m < s; m++) {
+        var c = Math.floor(i / s),
+            g = c * m,
+            h = i - c * (m + 1) - l;
+        0 == m ? (c += l) : (g += l), a.drawImage(e, 0, h, o, c, 0, g, o, c);
+    }
+    $(e).addClass('hide');
+}
+function showBig(e, t, a) {
+    var n = document.getElementById('popup');
+    if (
+        ((n.onclick = function () {
+            n.style.display = 'none';
+        }),
+        t || (t = !1),
+        t)
+    ) {
+        var d;
+        0 != n.getElementsByTagName('img').length &&
+            ((y = n.getElementsByTagName('img')[0]), n.removeChild(y)),
+            0 == n.getElementsByTagName('canvas').length
+                ? ((d = document.createElement('canvas')), n.append(d))
+                : (d = n.getElementsByTagName('canvas')[0]);
+        var i = d.getContext('2d'),
+            r = e.naturalWidth,
+            s = e.naturalHeight;
+        (d.width = r),
+            (d.height = s),
+            window.screen.width < 991
+                ? (d.style.width = window.screen.width + 'px')
+                : r > window.screen.width && (d.style.width = r / 2 + 'px'),
+            (d.style.padding = '10px');
+        var l = a.split('.');
+        l = l[0];
+        for (var o = get_num(aid, l), m = parseInt(s % o), c = 0; c < o; c++) {
+            var g = r,
+                h = Math.floor(s / o),
+                p = h * c,
+                u = s - h * (c + 1) - m;
+            0 == c ? (h += m) : (p += m), i.drawImage(e, 0, u, g, h, 0, p, g, h);
+        }
+    } else {
+        var y;
+        0 == n.getElementsByTagName('img').length
+            ? ((y = document.createElement('img')), n.append(y))
+            : (y = n.getElementsByTagName('img')[0]),
+            (y.src = e);
+    }
+    n.style.display = 'block';
+}
+function get_num(e, t) {
+    var a = 10;
+    if (e >= 268850) {
+        var n = e + t;
+        switch (((n = (n = (n = md5(n)).substr(-1)).charCodeAt()), (n %= 10))) {
+            case 0:
+                a = 2;
+                break;
+            case 1:
+                a = 4;
+                break;
+            case 2:
+                a = 6;
+                break;
+            case 3:
+                a = 8;
+                break;
+            case 4:
+                a = 10;
+                break;
+            case 5:
+                a = 12;
+                break;
+            case 6:
+                a = 14;
+                break;
+            case 7:
+                a = 16;
+                break;
+            case 8:
+                a = 18;
+                break;
+            case 9:
+                a = 20;
+        }
+    }
+    return a;
+}
